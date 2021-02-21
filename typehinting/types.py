22a8385155a5d28609bf638b1524acfcd684649a
aniks23 <aniket.purohit35@gmail.com>
2021-02-21 11:16:10 +0530

    [type hinting example added]

---
+++ typehinting/types.py
@@ -0,0 +1,12 @@
+def func(a: int) -> str:
+    return 34
+
+
+print(func(23))
+
+
+"""
+Python is a dynamically typed language. Hence, adding the type check won't work.
+
+To add strict type check, mypy needs to be installed.
+"""
